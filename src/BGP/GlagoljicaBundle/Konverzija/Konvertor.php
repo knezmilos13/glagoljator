914ad56df0f25fde83b5018b4b5d795ad61ba096
knezmilos13 <knezmilos13@gmail.com>
2015-04-21 23:28:47 +0200

    Konvertor prebacen iz interfejsa u apstraktnu klasu da bih mogao da definisem default implementaciju jedne metode (koja ce verovatno sluziti svrsi uvek)

--- src/BGP/GlagoljicaBundle/Konverzija/Konvertor.php
+++ src/BGP/GlagoljicaBundle/Konverzija/Konvertor.php
@@ -5,7 +5,7 @@ namespace BGP\GlagoljicaBundle\Konverzija;
 /** 
  * Zajednicki interfejs za sve klase koje rade konverziju iz jednog string zapisa u drugi.
  */
-interface Konvertor {
+abstract class Konvertor {
 
 	/**
 	 * Vraca niz svih ulaza koje ovaj Konvertor moze da konvertuje. Ulazi su oblika:
@@ -13,7 +13,7 @@ interface Konvertor {
 	 * 
 	 * @return array (vidi iznad)
 	 */
-	public function dajSveDozvoljeneUlaze();
+	abstract public function dajSveDozvoljeneUlaze();
 	
 	/**
 	 * Vraca niz svih izlaza koje ovaj Konvertor moze da generise za zadati ulaz. Izlazi su oblika:
@@ -23,7 +23,7 @@ interface Konvertor {
 	 *     identifikujuci string ulaza ciji se moguci izlazi traze ili asoc. niz sa stringom pod 'id'
 	 * @return array (vidi iznad)
 	 */
-	public function dajSveIzlazeZaUlaz($ulaz);
+	abstract public function dajSveIzlazeZaUlaz($ulaz);
 
 	/**
 	 * Vraca niz svih izlaza koje ovaj Konvertor moze da generise za zadati ulaz. Izlazi su oblika:
@@ -33,7 +33,15 @@ interface Konvertor {
 	 *     niz id-stringova ulaza ciji se moguci izlazi traze, ili niz asoc. nizova koji imaju string pod 'id'
 	 * @return array (vidi iznad)
 	 */
-	public function dajSveIzlazeZaUlaze($ulazi);
+	public function dajSveIzlazeZaUlaze($ulazi) {
+		$sviIzlazi = [ ];
+		foreach($ulazi as $ulaz) {
+			$jedniIzlazi = $this->dajSveIzlazeZaUlaz($ulaz);
+			$sviIzlazi = array_merge($sviIzlazi, $jedniIzlazi);
+		}
+		
+		return __($sviIzlazi)->uniq(false, function($izlaz) { return $izlaz['id']; });
+	}
 	
 	/**
 	 * Vraca niz parova [ ulaz, izlaz ] koji odgovaraju svim konverzijama koje zna da uradi.
@@ -42,14 +50,14 @@ interface Konvertor {
 	 * 
 	 * @return array (vidi iznad)
 	 */
-	public function dajSveParoveUlazIzlaz();
+	abstract public function dajSveParoveUlazIzlaz();
 	
 	/**
-	 * @param string $tipUlaza  - identifikujuci string ulaza (npr. "cirilica")
+	 * @param string $tipUlaza  - identifikujuci string ulaza (npr. "srp-cir")
 	 * @param string $ulaz      - ulaz kojeg treba konvertovati 
-	 * @param string $tipIzlaza - identifikujuci string izlaza (npr. "glagoljica")
+	 * @param string $tipIzlaza - identifikujuci string izlaza (npr. "srp-gla")
 	 */
-	public function konvertuj($tipUlaza, $ulaz, $tipIzlaza);
+	abstract public function konvertuj($tipUlaza, $ulaz, $tipIzlaza);
 	
 }
 
