45b543f7f7d253174a868aa8811a7427b790b2c2
knezmilos13 <knezmilos13@gmail.com>
2015-04-21 23:29:04 +0200

    Dodata klasa sa konstantama za konverziju

---
+++ src/BGP/GlagoljicaBundle/Konverzija/UlaziIzlazi.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace BGP\GlagoljicaBundle\Konverzija;
+
+/**
+ * Spisak svih ulaza/izlaza
+ */
+class UlaziIzlazi {
+	
+	public static $SR_CIRILICA = [ 'id' => 'srp-cir', 'ime' => 'Ćirilica' ];
+	public static $SR_LATINICA = [ 'id' => 'srp-lat', 'ime' => 'Latinica' ];
+	public static $SR_GLAGOLJICA = [ 'id' => 'srp-gla', 'ime' => 'Glagoljica' ]; 
+	
+}
+?>
