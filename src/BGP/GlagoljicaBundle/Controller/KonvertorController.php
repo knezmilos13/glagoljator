ccbae6ed0a0fe25d5016a9d24380788786eb6cbf
knezmilos13 <knezmilos13@gmail.com>
2015-04-22 15:56:59 +0200

    Osposobljena stranica za konverziju (samo cirilica/latinica, naravno)

--- src/BGP/GlagoljicaBundle/Controller/KonvertorController.php
+++ src/BGP/GlagoljicaBundle/Controller/KonvertorController.php
@@ -3,14 +3,20 @@
 namespace BGP\GlagoljicaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use BGP\GlagoljicaBundle\Konverzija\KonvertorMenadzer;
+use BGP\GlagoljicaBundle\Konverzija\CirLatKonvertor;
 
 class KonvertorController extends Controller {
     public function indexAction() {
+    	$konvertorMenadzer = new KonvertorMenadzer();
+		$konvertorMenadzer->dodajKonvertor(new CirLatKonvertor());
+		
         return $this->render('BGPGlagoljicaBundle:Konvertor:index.html.twig', [
 			'jezik' => 'sr',
 			'metaDescription' => '',
 			'metaTitle' => 'Glagoljica - Konverzija u glagoljicu',
-			'title' => 'Glagoljica - Konverzija u glagoljicu'
+			'title' => 'Glagoljica - Konverzija u glagoljicu',
+			'ulazi' => $konvertorMenadzer->dajSveUlaze()
 		]);
     }
 }
