101ee952d9a1713fb4e31764b206f6399fbbb7b4
knezmilos13 <knezmilos13@gmail.com>
2015-05-21 21:14:46 +0200

    Dodata dodatna polja u sonata admin

--- src/BGP/GlagoljicaBundle/Admin/ClanakAdmin.php
+++ src/BGP/GlagoljicaBundle/Admin/ClanakAdmin.php
@@ -13,17 +13,22 @@ class ClanakAdmin extends Admin {
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
-            ->add('naslov', 'text', array('label' => 'Naslov članka'))
-            ->add('tekst', 'text', array('label' => 'Tekst članka'));
+            ->add('redosled', 'number', [ 'label' => 'Redna pozicija članka u glavnom meniju' ])
+			->add('naslov', 'text', [ 'label' => 'Naslov članka (biće prikazan nad tekstom)' ])
+			->add('meniNaslov', 'text', [ 'label' => 'Kratak naslov članka koji će biti prikazan u glavnom meniju. Može biti isti kao naslov.'] )
+			->add('metaNaslov', 'text', [ 'label' => 'Naslov članka' ])
+			->add('metaDescription', 'text', [ 'label' => 'Tekst koji će ići u meta "description" tag stranice kad se prikaže članak' ])
+			->add('slug', 'text', [ 'label' => 'Slug - biće nakačen na URL stranice. Najbolje da bude isto kao naslov, samo malim slovima i sa crtama (-) umesto razmaka.' ])
+            ->add('tekst', 'textarea', [ 'label' => 'Tekst članka' ]);
     }
 
     // Fields to be shown on filter forms
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
         $datagridMapper
-            ->add('naslov')
-            ->add('tekst')
-        ;
+        	->add('redosled')
+			->add('naslov')
+			->add('meniNaslov');
     }
 
     // Fields to be shown on lists
@@ -31,7 +36,8 @@ class ClanakAdmin extends Admin {
     {
         $listMapper
             ->addIdentifier('id')
+			->add('redosled')
             ->add('naslov')
-            ->add('tekst');
+            ->add('meniNaslov');
     }
 }
