21c512f40f20107f18eebb1c05466919de165b94
knezmilos13 <knezmilos13@gmail.com>
2015-05-22 11:21:13 +0200

    Dodati help natpisi i dodato sortiranje po polju redosled.

--- src/BGP/GlagoljicaBundle/Admin/ClanakAdmin.php
+++ src/BGP/GlagoljicaBundle/Admin/ClanakAdmin.php
@@ -10,21 +10,19 @@ use Sonata\AdminBundle\Form\FormMapper;
 class ClanakAdmin extends Admin {
 	
     // Fields to be shown on create/edit forms
-    protected function configureFormFields(FormMapper $formMapper)
-    {
+    protected function configureFormFields(FormMapper $formMapper) {
         $formMapper
             ->add('redosled', 'number', [ 'label' => 'Redna pozicija članka u glavnom meniju' ])
-			->add('naslov', 'text', [ 'label' => 'Naslov članka (biće prikazan nad tekstom)' ])
-			->add('meniNaslov', 'text', [ 'label' => 'Kratak naslov članka koji će biti prikazan u glavnom meniju. Može biti isti kao naslov.'] )
-			->add('metaNaslov', 'text', [ 'label' => 'Naslov članka' ])
-			->add('metaDescription', 'text', [ 'label' => 'Tekst koji će ići u meta "description" tag stranice kad se prikaže članak' ])
-			->add('slug', 'text', [ 'label' => 'Slug - biće nakačen na URL stranice. Najbolje da bude isto kao naslov, samo malim slovima i sa crtama (-) umesto razmaka.' ])
+			->add('naslov', 'text', [ 'label' => 'Naslov članka', 'help' => 'Biće prikazan nad tekstom, u h1' ])
+			->add('meniNaslov', 'text', [ 'required' => false, 'label' => 'Kratak naslov', 'help' => 'Biće prikazan u glavnom meniju. Potencijalno isti kao naslov. Ako se ne stavi ništa ovde, biće prekopirana vrednost naslova'] )
+			->add('metaNaslov', 'text', [ 'required' => false, 'label' => 'Meta naslov', 'help' => 'Tekst koji će ići u meta "title" tag stranice kad se prikaže članak. Ako se ne stavi ništa ovde, biće prekopirana vrednost naslova' ])
+			->add('metaDescription', 'text', [ 'required' => false, 'label' => 'Meta description', 'help' => 'Tekst koji će ići u meta "description" tag stranice kad se prikaže članak.' ])
+			->add('slug', 'text', [ 'label' => 'Slug', 'help' => 'Biće nakačen na URL stranice. Najbolje da bude isto kao naslov, samo malim slovima i sa crtama (-) umesto razmaka.' ])
             ->add('tekst', 'textarea', [ 'label' => 'Tekst članka' ]);
     }
 
     // Fields to be shown on filter forms
-    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
-    {
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper) {
         $datagridMapper
         	->add('redosled')
 			->add('naslov')
@@ -32,12 +30,18 @@ class ClanakAdmin extends Admin {
     }
 
     // Fields to be shown on lists
-    protected function configureListFields(ListMapper $listMapper)
-    {
+    protected function configureListFields(ListMapper $listMapper) {
         $listMapper
             ->addIdentifier('id')
 			->add('redosled')
             ->add('naslov')
             ->add('meniNaslov');
     }
+	
+	/** Redosled listanja odredjuje */
+    protected $datagridValues = array(
+        '_page' => 1,
+        '_sort_order' => 'ASC',
+        '_sort_by' => 'redosled'
+    );
 }
